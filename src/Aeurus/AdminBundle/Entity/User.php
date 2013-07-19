ea2a6586775714e0146fe59888809c7e4fbdb6f2
Rodrigo Campos <rodrigocampos@udec.cl>
2013-07-19 15:50:29 -0400

    integrado FOSUserBundle

---
+++ src/Aeurus/AdminBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+// src/Aeurus/AdminBundle/Entity/User.php
+
+namespace Aeurus\AdminBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
